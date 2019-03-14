7dc1176416b3c02dd4901bbe912e13156c937b55
flowmaster20 <flowmaster2030>
2019-03-14 19:56:16 +0100

    kodowanie dziala ale trzeba zrobic reverse

--- 2k12/maj/zadanie 4.cpp
+++ 2k12/maj/zadanie 4.cpp
@@ -14,25 +14,44 @@ string wynik;
 cin>> slowo;
 cin >> klucz;
 
-
+wynik = "";
 
 for (int i =0 ;i < slowo.size();i++){
-i = k;
-if (i = klucz.size()) k == 0;
-znak = zamien(slowo[i]);
-znakklucz = zamien(klucz[k]);
 
-wynik = wynik +
+//jak zapetlac dlugosc klucza kiedy slowo jest od niego dluzsze
+
+int k = i % klucz.size(); //czy to dziala jako zapetlenie dzialania k w granicahc dlugosci klucza
+
+int znak = zamien(slowo[i]);
+int znakklucz = zamien(klucz[k]) - 64;
+int znakwynik = znakklucz + znak;
+if (znakwynik > 90)znakwynik = znakwynik -26;
+char koniec = znakwynik;
+
+wynik = wynik + koniec;
 
 
 }
 
+ cout << wynik<<endl;
+
+
+ for (int i = 0;i< slowo.size();i++){
+ int k = i % klucz.size();
+ int znak = zamien(slowo[i]);
+ int znakklucz = zamien(klucz[k]) - 64;
+ int znakwynik = znak - znakklucz;
+ if (znakwynik < 65)znakwynik = znakwynik +26;
+ char koniec = znakwynik;
+ wynik = wynik + koniec;
+ }
+ cout << wynik;
 
 }
 
-zamien (char znak){
+int zamien (char znak){
 
-int znakliczba = znak
+int znakliczba = znak;
 
 return znakliczba;
 
