daed1bd9dd2594a0ee0e898568b9694f2caecf6d
laptop <flowmaster2030@gmail.com>
2019-03-14 17:36:26 +0100

    dodaje mature z maja 2k12 i poczatek programu ztego roku

---
+++ 2k12/maj/zadanie 4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+
+string slowo;
+string klucz;
+string wynik;
+
+cin>> slowo;
+cin >> klucz;
+
+
+
+for (int i =0 ;i < slowo.size();i++){
+
+char literaslowo = slowo[i]; //nwm czy to zadziala ale to jest jedyny prosty sposob jaki sam wymslilem
+
+int numerlitera = literaslowo;
+
+}
+
+
+}
