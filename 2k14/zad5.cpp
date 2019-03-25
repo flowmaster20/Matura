dfdf9277d87461cf2ff43dd11dd4759163ce5e89
mateusz <mkojro20@gmail.com>
2019-03-25 21:41:29 +0100

    zadanie  5 cale  2k14

--- 2k14/zad5.cpp
+++ 2k14/zad5.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <vector>
 
 using namespace std;
-
+int ile [1000]={0};
 int main (){
-	
 	int licza = 0;
+	int liczb = 0;
+
+
+	vector<string> slowa;
+
 
 bool prim(int number);
-
+bool rise(string slowo);
+bool czyBylo(string slowo,vector <string> slowa);
 
 fstream p1;
 
 p1.open("NAPIS.TXT",ios::in);
 
 for (int i = 0 ;i < 1000 ;i++){
-	
-	
+
+
+
 	string napis;
 	int napisint = 0;
 
@@ -25,32 +32,49 @@ for (int i = 0 ;i < 1000 ;i++){
 
 	for (int x = 0;x< napis.size();x++){
 
-		cout << napis ;
 
 		char znak = napis[x];
-		cout << znak<<"  ";
 		napisint = napisint + znak;
 
 
 	}
-	cout << endl;
 
 	if (prim(napisint)==true) {licza++;}
+    if (rise(napis)==true) {liczb++;}
+        if (czyBylo(napis,slowa)==false) {
+
+
+            slowa.push_back(napis);
+
+
+        }
+        else{
+
+            cout << napis << endl;
+
+        }
+
+
 
 
 
 }
 
 
-cout << licza;
+cout << licza<<endl;
+cout << liczb;
+for (int i =0 ; i < slowa.size();i++){
+    if (ile[i]>1){
+    cout << slowa[i]<< endl;
+    }
 
-
+}
 }
 
 
 
 bool prim(int number){
-	
+
 
 for (int x = 2 ;x < number ;x++){
 
@@ -58,8 +82,8 @@ for (int x = 2 ;x < number ;x++){
 	if (number % x == 0){
 
 
-	return false;  
-	
+	return false;
+
 
 	}
 
@@ -68,4 +92,45 @@ for (int x = 2 ;x < number ;x++){
 
 return true;
 
+}
+
+bool rise(string slowo){
+
+
+for (int i =0 ;i < slowo.size()-1;i++){
+
+
+    char znak1 = slowo[i];
+    char znak2 = slowo[i+1];
+
+    if (znak1 >= znak2){
+
+        return false;
+
+    }
+
+
+}
+
+return true;
+}
+
+
+
+bool czyBylo(string slowo, vector<string> slowa){
+
+
+    for (int x =0;x < slowa.size();x++){
+
+
+        if (slowo == slowa[x]){
+            ile[x]=ile[x]+1;
+            return true;
+
+        }
+
+
+    }
+
+return false;
 }
