30e73cfb6c9611d7c50a26094885e17d4fa992c8
mateusz <mkojro20@gmail.com>
2019-03-28 17:59:01 +0100

    dodalem template sortowania

--- templates/sortowanie.cpp
+++ templates/sortowanie.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 string sortuj(string a);
 
 int main(){
 
+string a = "11221212";
 
+cout << sortuj(a);
 
 }
 
 string sortuj(string a){
 
 
+for (int i = 0;i < a.size();i++){
+
+        for (int j = i ;j < a.size();j++){
 
 
+            if (a[i] < a[j]){
+
+                swap(a[i],a[j]);
+
+            }
+
+
+        }
+
+
+}
+
+        return a;
 
 }
