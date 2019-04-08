b97ad37278f5dee5619880ed122786f33459c3bd
mateusz <mkojro20@gmail.com>
2019-04-08 12:50:42 +0200

    podrecznik z  informatyki dane

---
+++ 2k15_zbior/zad.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int maxl = 0;
+int licz = 0;
+int dif =0;
+
+int tab[310];
+
+int main(){
+
+fstream p1;
+p1.open("dziennik.txt",ios::in);
+
+for (int i = 0;i < 310;i++){
+
+    p1 >> tab[i];
+
+}
+
+for(int i = 0;i < 309;i++){
+
+    int dlugosc = 1;
+
+    int k= 1;
+
+    while(tab[i + k-1]<tab[i + k]){
+
+        dlugosc++;
+        cout << dlugosc << endl;
+        k++;
+
+    }
+    if (maxl < dlugosc){
+
+        maxl = dlugosc;
+        dif = tab[i + k] - tab[i];
+
+    }
+    if (dlugosc > 3){
+
+        licz++;
+
+    }
+
+    i += k -1;
+}
+
+cout << endl<<licz;
+
+}
