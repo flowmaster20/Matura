925f7062ea4130ce0c2563b382676664e42e5214
mateusz <mkojro20@gmail.com>
2019-04-12 14:22:26 +0200

    zadanie 59 algorytm ich

--- podrecznik/59/zad59-3.cpp
+++ podrecznik/59/zad59-3.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <string>
-#include <cmath>
+//#include <cmath>
 #include <math.h>
 
 using  namespace std;
@@ -14,25 +14,28 @@ int main (){
 
 fstream p1;
 p1.open("59.txt",ios::in);
-string liczba;
-cin >> liczba;
+
+
+cout << endl<<endl<<w(123);
+
+}
+int w(int liczba){
+    int k = 0;
+while(pow(10,k)< liczba){
+    k++;
+
+}
 int iloczyn = 1;
-
-for(int i = 0 ;i < liczba.size();i++){
-
-   int znak = liczba[i];
-   znak -= 48;
-   cout << znak<<endl;
-
-   iloczyn *= znak;
-
-
+for(int i = k-1;i >=0;i--){
+int    potega = pow(10,i);
+    iloczyn *= liczba /potega;
+    liczba = liczba % (potega);
+cout << potega << endl;
 }
 
-cout << endl<<endl<<iloczyn;
+return iloczyn;
 
 }
-
 
 
 
